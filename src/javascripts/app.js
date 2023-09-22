4486bbf6269ab8a973b4cbb3838e6b375e8d06b6
Rony Lee <rongyil33@gmail.com>
2023-09-22 16:08:02 +0800

    Add more pages

--- src/javascripts/app.js
+++ src/javascripts/app.js
@@ -1,2 +1,7 @@
 // alert("Hello World!");
-const arrow = document.querySelector("#arrow");
+const arrow = document.querySelector("#arrow-group");
+const intro = document.querySelector("#intro");
+
+arrow.addEventListener("click", () => {
+  intro.scrollIntoView({ behavior: "smooth" });
+});
